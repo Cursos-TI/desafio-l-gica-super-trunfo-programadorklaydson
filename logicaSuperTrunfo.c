34dffaca213fc0282665ed63fdb62206071536f2
programadorklaydson <edvaldoazevedo097@gmail.com>
2025-04-24 14:10:04 +0000

    criei o if, else para a comparação dos atributos das cartas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -39,5 +39,109 @@ int main() {
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
+
+
+
+
+    //cadastramento das variaveis da carta 1:
+
+    char Estado, codigodacarta[15], nome[15];
+    int populacaoA, pontosturisticosA;
+    float areaA, PIBa; 
+    
+    // cadastramento da carta 1:
+    
+    printf("Carta 1:\n");    
+    
+    printf("Digite a letra do Estado.\n");
+    scanf("%s", &Estado);
+    
+    printf("Digite o codigo da carta.\n");
+    scanf("%s", &codigodacarta);
+    
+    printf("Digite o primeiro nome da cidade.\n");
+    scanf("%s", &nome);
+    
+    printf("Digite a sua população.\n");
+    scanf("%d", &populacaoA);
+    
+    printf("Digite o tamanho da area.\n");
+    scanf("%f", &areaA);
+    
+    printf("Digite o PIB da cidade.\n");
+    scanf("%f", &PIBa);
+    
+    printf("Digite a quantidade de pontos turisticos.\n");
+    scanf("%d", &pontosturisticosA);
+    
+    printf("\n");
+    
+    // cadastramento das variaveis da carta 2:
+    
+    char estado, Codigodacarta[15], Nome[15];
+    int PopulacaoB,pontosturisticosB;
+    float AreaB, pibB; 
+    
+    // cadastramento da carta 2:
+
+    printf("Carta 2:\n");    
+    
+    printf("Digite o Estado.\n");
+    scanf("%s", &estado);
+    
+    printf("Digite o codigo da carta.\n");
+    scanf("%s", &Codigodacarta);
+    
+    printf("Digite o primeiro nome da cidade.\n");
+    scanf("%s", &Nome);
+    
+    printf("Digite a sua população.\n");
+    scanf("%d", &PopulacaoB);
+    
+    printf("Digite o tamanho da area.\n");
+    scanf("%f", &AreaB);
+    
+    printf("Digite o PIB da cidade.\n");
+    scanf("%f", &pibB);
+    
+    printf("Digite a quantidade de pontos turisticos.\n");
+    scanf("%d", &pontosturisticosB);
+    
+    printf("\n");
+    
+    //resultado do cadastro da carta 1:
+    
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", Estado);
+    printf("Codigo da carta: %s\n", codigodacarta);
+    printf("Nome da cidade: %s\n", nome);
+    printf("Popucação: %d\n", populacaoA);
+    printf("Area: %f\n", areaA);
+    printf("PIB: %f\n", PIBa);
+    printf("Pontos turisticos: %d\n", pontosturisticosA);
+    
+    printf("\n");
+    
+    //resultado do cadastro da carta 2:
+    
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado);
+    printf("Codigo da carta: %s\n", Codigodacarta);
+    printf("Nome da cidade: %s\n", Nome);
+    printf("Popucação: %d\n", PopulacaoB);
+    printf("Area: %f\n", AreaB);
+    printf("PIB: %f\n", pibB);
+    printf("Pontos turisticcos: %d\n", pontosturisticosB);
+    
+    printf("\n");
+
+    // conparação dos atributos das cartas 1 e 2: 
+
+    if (areaA > AreaB){
+        printf("### A carta 1 VENCEU em extensão territorial! ###\n");
+    } else {
+        printf("### A carta 2 VENCEU em extensão territorial! ###\n");
+    }
+
     return 0;
 }
